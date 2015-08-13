4425ac109724d63a14516b7f253f627d73378581
Mikołaj Baranowski <mikolajb@gmail.com>
2015-08-13 23:32:16 +0200

    First!

---
+++ README.md
@@ -0,0 +1,36 @@
+# Wallpaper switch
+
+Every day a new picture from: [Astronomy Picture of the Day](http://apod.nasa.gov/apod/astropix.html)
+
+## Content of the repository
+
+- `wallpaper-switch.go` - the acctual application
+- `wallpaper-switch.service` - systemd service
+- `wallpaper-switch.timer` - systemd timer
+
+## How it works
+
+`wallpaper-switch` checks the RSS feed from NASA's [Astronomy Picture of the Day](http://apod.nasa.gov/apod/astropix.html) and, for a new content, sets a desktop background using `gsettings` command.
+
+It stores its status (file `status.toml`) and the current desktop wallpaper in `"XDG_DATA_HOME/wallpaper-switch"` or, if the envrionment variable is not set, in `".local/share/wallpaper-switch"`.
+
+## How to install
+
+- Install dependencies:
+
+`go get -u github.com/naoina/toml`
+
+`go get -u github.com/nu7hatch/gouuid`
+
+- compile:
+
+`go build wallpaper-switch.go`
+
+- place files `wallpaper-switch.timer` and `wallpaper-switch.service` in `~/.local/share/systemd/user`
+
+- modify variable `ExecStart` in `wallpaper-switch.service` - it should point to a binary of wallpaper-switch.
+
+Then, execute the following commands:
+
+`systemctl --user enable wallpaper-switch.timer`
+`systemctl --user start wallpaper-switch.timer`
---
+++ wallpaper-switch.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+
+	"github.com/naoina/toml"
+	uuid "github.com/nu7hatch/gouuid"
+	"golang.org/x/net/html"
+)
+
+const (
+	AppName         = "wallpaper-switch"
+	StateFileName   = "status.toml"
+	PictureFileName = "background"
+	NasaRSS         = "http://apod.nasa.gov/apod.rss"
+)
+
+type State struct {
+	LastModification time.Time
+	SourceURL        string
+	PictureFilePath  string
+}
+
+type StateFile struct {
+	State    *State
+	FilePath string
+}
+
+func (c *StateFile) loadState() *State {
+	buf, err := ioutil.ReadFile(c.FilePath)
+
+	c.State = new(State)
+	if err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
+
+	if err := toml.Unmarshal(buf, c.State); err != nil {
+		fmt.Println("Problem with reading configuration file")
+	}
+
+	return c.State
+}
+
+func (c *StateFile) storeState(state *State) {
+	c.State = state
+
+	data, err := toml.Marshal(*state)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = ioutil.WriteFile(c.FilePath, data, 0600); err != nil {
+		panic(err)
+	}
+}
+
+func getDirectories() (string, string) {
+	config_home := os.Getenv("XDG_CONFIG_HOME")
+	data_home := os.Getenv("XDG_DATA_HOME")
+
+	if len(config_home) == 0 {
+		config_home = path.Join(os.Getenv("HOME"), ".config")
+	}
+
+	if len(data_home) == 0 {
+		data_home = path.Join(os.Getenv("HOME"), ".local/share")
+	}
+
+	config_home = path.Join(config_home, AppName)
+	data_home = path.Join(data_home, AppName)
+
+	if _, err := os.Stat(config_home); os.IsNotExist(err) {
+		if err = os.MkdirAll(config_home, 0700); err != nil {
+			panic(err)
+		}
+	}
+
+	if _, err := os.Stat(data_home); os.IsNotExist(err) {
+		if err = os.MkdirAll(data_home, 0700); err != nil {
+			panic(err)
+		}
+	}
+
+	return config_home, data_home
+}
+
+func main() {
+	_, data_home := getDirectories()
+	state_file := new(StateFile)
+	state_file.FilePath = path.Join(data_home, StateFileName)
+	state := state_file.loadState()
+
+	type Item struct {
+		Title string `xml:"title"`
+		Link  string `xml:"link"`
+	}
+
+	type Channel struct {
+		Items []Item `xml:"channel>item"`
+	}
+
+	rss_resp, err := http.Get(NasaRSS)
+
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(rss_resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	rss := Channel{}
+	xml.Unmarshal(body, &rss)
+
+	item_resp, err := http.Get(rss.Items[0].Link)
+
+	if err != nil {
+		panic(err)
+	}
+
+	url, err := url.Parse(rss.Items[0].Link)
+
+	if err != nil {
+		panic(err)
+	}
+
+	doc, err := html.Parse(item_resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var f func(*html.Node) *string
+	f = func(n *html.Node) *string {
+		if n.Type == html.ElementNode && n.Data == "img" {
+			return &n.Parent.Attr[0].Val
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			r := f(c)
+			if r != nil {
+				return r
+			}
+		}
+		return nil
+	}
+
+	url.Path = path.Join(path.Dir(url.Path), *f(doc))
+
+	if url.String() == state.SourceURL {
+		fmt.Println("Same file, not changeing")
+		return
+	}
+
+	picture_resp, err := http.Get(url.String())
+
+	u4, err := uuid.NewV4()
+
+	if err != nil {
+		panic(err)
+	}
+
+	picture_path := path.Join(data_home,
+		fmt.Sprintf("%s-%s%s",
+			PictureFileName,
+			u4.String(),
+			path.Ext(url.Path)))
+	picture_file, err := os.Create(picture_path)
+
+	defer picture_file.Close()
+
+	_, err = io.Copy(picture_file, picture_resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	picture_file.Sync()
+
+	cmds := []*exec.Cmd{
+		exec.Command("gsettings", "set", "org.gnome.desktop.background",
+			"picture-uri", picture_path),
+		exec.Command("gsettings", "set", "org.gnome.desktop.screensaver",
+			"picture-uri", picture_path),
+		exec.Command("notify-send", "New Wallpaper", rss.Items[0].Link)}
+
+	for _, cmd := range cmds {
+		err = cmd.Run()
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	os.Remove(state.PictureFilePath)
+
+	state.LastModification = time.Now()
+	state.SourceURL = url.String()
+	state.PictureFilePath = picture_path
+	state_file.storeState(state)
+}
